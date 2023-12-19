7e4e954e1ae4daedeaeb084cd6d5190ff5da6504
alexia <20200992@uaz.edu.mx>
2023-12-19 17:50:11 -0600

    loading

--- src/Routes/Users.jsx
+++ src/Routes/Users.jsx
@@ -1,19 +1,27 @@
 import React, { useEffect, useRef, useState } from 'react'
 import UsersContainer from '../components/UsersContainer';
 import { Input } from 'postcss';
+import Loading from '../components/Loading';
 
 const Users = () => {
     const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(null);
     let BaseUrl = "https://api.github.com/users";
     const user = useRef('')
 
     async function Allusers() {
-        const res = await fetch(BaseUrl);
-        const data = await res.json();
-        setUsers(data)
+        if (user.current.value === "") {
+            setLoading(true)
+            const res = await fetch(BaseUrl);
+            const data = await res.json();
+            setUsers(data)
+            setLoading(null)
+        }
+
     }
 
     async function findUser() {
+        setLoading(true)
         if (user.current.value !== '') {
             setUsers("")
             const res = await fetch(BaseUrl + "/" + user.current.value);
@@ -24,6 +32,7 @@ const Users = () => {
         } else {
             Allusers();
         }
+        setLoading(null)
     }
     useEffect(() => {
         Allusers();
@@ -40,7 +49,7 @@ const Users = () => {
                 </button>
             </div>
 
-            <UsersContainer users={users} />
+            {loading ? <Loading /> : <UsersContainer users={users} />}
         </div>
     )
 }
